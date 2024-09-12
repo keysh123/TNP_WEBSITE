6f9bb7c2b0b7c3f74238cf291e8d2d11ba3ba86f
keysh123 <keyashah126@gmail.com>
2024-09-12 11:45:58 +0530

    added frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import StudentForm from './components/student/StudentForm';
 
 // basic url=http://localhost:3030/
 function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StudentForm/>
     </div>
   );
 }
